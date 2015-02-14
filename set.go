9d790df12470f462b8b1ac42ad3cbc50bbf20da4
张耐国 <seefan@126.com>
2015-02-14 21:45:23 +0800

    增加删除 key 的方法

--- set.go
+++ set.go
@@ -9,7 +9,7 @@ import (
 //  key 键值
 //  val 存贮的 value 值,val只支持基本的类型，如果要支持复杂的类型，需要开启连接池的 Encoding 选项
 //  ttl 可选，设置的过期时间，单位为秒
-//  返回可能的错误
+//  返回 err，可能的错误，操作成功返回 nil
 func (this *Client) Set(key string, val interface{}, ttl ...int) (err error) {
 	var resp []string
 	if len(ttl) > 0 {
@@ -29,8 +29,8 @@ func (this *Client) Set(key string, val interface{}, ttl ...int) (err error) {
 //获取指定 key 的值内容
 //
 //  key 键值
-//  返回一个 Value,可以方便的向其它类型转换
-//  返回一个可能的错误
+//  返回 一个 Value,可以方便的向其它类型转换
+//  返回 一个可能的错误，操作成功返回 nil
 func (this *Client) Get(key string) (Value, error) {
 	resp, err := this.Client.Do("get", key)
 	if err != nil {
@@ -47,7 +47,7 @@ func (this *Client) Get(key string) (Value, error) {
 //  key 要设置过期的 key
 //  ttl 存活时间(秒)
 //  返回 re，设置是否成功，如果当前 key 不存在返回 false
-//  返回 err，执行的错误
+//  返回 err，执行的错误，操作成功返回 nil
 func (this *Client) Expire(key string, ttl int) (re bool, err error) {
 	resp, err := this.Do("expire", key, ttl)
 	if err != nil {
@@ -63,7 +63,7 @@ func (this *Client) Expire(key string, ttl int) (re bool, err error) {
 //
 //  key 要查询的 key
 //  返回 re，如果当前 key 不存在返回 false
-//  返回 err，执行的错误
+//  返回 err，执行的错误，操作成功返回 nil
 func (this *Client) Exists(key string) (re bool, err error) {
 	resp, err := this.Do("exists", key)
 	if err != nil {
@@ -74,4 +74,21 @@ func (this *Client) Exists(key string) (re bool, err error) {
 		return resp[1] == "1", nil
 	}
 	return false, goerr.New("执行过程中有错误，错误代码:%v", resp)
+}
+
+//删除指定 key
+//
+//  key 要删除的 key
+//  返回 err，执行的错误，操作成功返回 nil
+func (this *Client) Del(key string) error {
+	resp, err := c.Do("del", key)
+	if err != nil {
+		return err
+	}
+
+	//response looks like this: [ok 1]
+	if len(resp) > 0 && resp[0] == "ok" {
+		return nil
+	}
+	return goerr.New("执行过程中有错误，错误代码:%v", resp)
 }
