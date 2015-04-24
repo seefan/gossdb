4913f164cae203b57d915fd8b8d996a189daa089
张耐国 <seefan@126.com>
2015-04-24 16:44:56 +0800

    增加部分方法

--- set.go
+++ set.go
@@ -28,6 +28,24 @@ func (this *Client) Set(key string, val interface{}, ttl ...int) (err error) {
 	return makeError(resp, key)
 }
 
+//当 key 不存在时, 设置指定 key 的值内容. 如果已存在, 则不设置.
+//
+//  key 键值
+//  val 存贮的 value 值,val只支持基本的类型，如果要支持复杂的类型，需要开启连接池的 Encoding 选项
+//  返回 err，可能的错误，操作成功返回 nil
+//  返回 val 1: value 已经设置, 0: key 已经存在, 不更新.
+func (this *Client) Setnx(key string, val interface{}) (Value, error) {
+	resp, err := this.Client.Do("setnx", key, this.encoding(val, false))
+
+	if err != nil {
+		return "", goerr.NewError(err, "Setnx %s error", key)
+	}
+	if len(resp) > 0 && resp[0] == "ok" {
+		return Value(resp[1]), nil
+	}
+	return "", makeError(resp, key)
+}
+
 //获取指定 key 的值内容
 //
 //  key 键值
@@ -216,4 +234,154 @@ func (this *Client) MultiDel(key ...string) (err error) {
 		return nil
 	}
 	return makeError(resp, key)
+}
+
+////设置字符串内指定位置的位值(BIT), 字符串的长度会自动扩展.
+////
+////  key 键值
+////  offset 位偏移
+////  bit  0 或 1
+////  返回 val，原来的位值
+////  返回 err，可能的错误，操作成功返回 nil
+//func (this *Client) Setbit(key string, offset int64, bit byte) (byte, error) {
+
+//	resp, err := this.Client.Do("setbit", key, offset, bit)
+
+//	if err != nil {
+//		return 255, goerr.NewError(err, "Setbit %s error", key)
+//	}
+//	if len(resp) == 2 && resp[0] == "ok" {
+//		return Value(resp[1]).Byte(), nil
+//	}
+//	return 255, makeError(resp, key)
+//}
+
+////获取字符串内指定位置的位值(BIT).
+////
+////  key 键值
+////  offset 位偏移
+////  返回 val，位值
+////  返回 err，可能的错误，操作成功返回 nil
+//func (this *Client) Getbit(key string, offset int64) (byte, error) {
+
+//	resp, err := this.Client.Do("getbit", key, offset)
+
+//	if err != nil {
+//		return 255, goerr.NewError(err, "Getbit %s error", key)
+//	}
+//	if len(resp) == 2 && resp[0] == "ok" {
+//		return Value(resp[1]).Byte(), nil
+//	}
+//	return 255, makeError(resp, key)
+//}
+//获取字符串的子串.
+//
+//  key 键值
+//  start int, 子串的字节偏移;若 start 是负数, 则从字符串末尾算起.
+//  size  int,可选, 子串的长度(字节数), 默认为到字符串最后一个字节;若 size 是负数, 则表示从字符串末尾算起, 忽略掉那么多字节(类似 PHP 的 substr())
+//  返回 val，字符串的部分
+//  返回 err，可能的错误，操作成功返回 nil
+func (this *Client) Substr(key string, start int64, size ...int64) (val string, err error) {
+	var resp []string
+	if len(size) > 0 {
+		resp, err = this.Client.Do("substr", key, start, size[0])
+	} else {
+		resp, err = this.Client.Do("substr", key, start)
+	}
+
+	if err != nil {
+		return "", goerr.NewError(err, "Substr %s error", key)
+	}
+	if len(resp) > 1 && resp[0] == "ok" {
+		return resp[1], nil
+	}
+	return "", makeError(resp, key)
+}
+
+//计算字符串的长度(字节数).
+//
+//  key 键值
+//  返回 字符串的长度, key 不存在则返回 0.
+//  返回 err，可能的错误，操作成功返回 nil
+func (this *Client) Strlen(key string) (int64, error) {
+
+	resp, err := this.Client.Do("strlen", key)
+
+	if err != nil {
+		return -1, goerr.NewError(err, "Strlen %s error", key)
+	}
+	if len(resp) > 1 && resp[0] == "ok" {
+		return Value(resp[1]).Int64(), nil
+	}
+	return -1, makeError(resp, key)
+}
+
+//列出处于区间 (key_start, key_end] 的 key 列表.("", ""] 表示整个区间.
+//
+//  keyStart int 返回的起始 key(不包含), 空字符串表示 -inf.
+//  keyEnd int 返回的结束 key(包含), 空字符串表示 +inf.
+//  limit int 最多返回这么多个元素.
+//  返回 返回包含 key 的数组.
+//  返回 err，可能的错误，操作成功返回 nil
+func (this *Client) Keys(keyStart, keyEnd string, limit int64) ([]string, error) {
+
+	resp, err := this.Client.Do("keys", keyStart, keyEnd, limit)
+
+	if err != nil {
+		return nil, goerr.NewError(err, "Keys %s error", keyStart, keyEnd, limit)
+	}
+	if len(resp) > 0 && resp[0] == "ok" {
+		return resp[1:], nil
+	}
+	return nil, makeError(resp, keyStart, keyEnd, limit)
+}
+
+//列出处于区间 (key_start, key_end] 的 key-value 列表.("", ""] 表示整个区间.
+//
+//  keyStart int 返回的起始 key(不包含), 空字符串表示 -inf.
+//  keyEnd int 返回的结束 key(包含), 空字符串表示 +inf.
+//  limit int 最多返回这么多个元素.
+//  返回 返回包含 key 的数组.
+//  返回 err，可能的错误，操作成功返回 nil
+func (this *Client) Scan(keyStart, keyEnd string, limit int64) (map[string]Value, error) {
+
+	resp, err := this.Client.Do("scan", keyStart, keyEnd, limit)
+
+	if err != nil {
+		return nil, goerr.NewError(err, "Scan %s error", keyStart, keyEnd, limit)
+	}
+	if len(resp) > 0 && resp[0] == "ok" {
+		re := make(map[string]Value)
+		size := len(resp)
+		for i := 1; i < size-1; i += 2 {
+			re[resp[i]] = Value(resp[i+1])
+		}
+		return re, nil
+	}
+	return nil, makeError(resp, keyStart, keyEnd, limit)
+}
+
+//列出处于区间 (key_start, key_end] 的 key-value 列表, 反向顺序.("", ""] 表示整个区间.
+//
+//  keyStart int 返回的起始 key(不包含), 空字符串表示 -inf.
+//  keyEnd int 返回的结束 key(包含), 空字符串表示 +inf.
+//  limit int 最多返回这么多个元素.
+//  返回 返回包含 key 的数组.
+//  返回 err，可能的错误，操作成功返回 nil
+func (this *Client) Rscan(keyStart, keyEnd string, limit int64) (map[string]Value, error) {
+
+	resp, err := this.Client.Do("rscan", keyStart, keyEnd, limit)
+
+	if err != nil {
+		return nil, goerr.NewError(err, "Rscan %s error", keyStart, keyEnd, limit)
+	}
+	if len(resp) > 0 && resp[0] == "ok" {
+		re := make(map[string]Value)
+		size := len(resp)
+		for i := 1; i < size-1; i += 2 {
+			re[resp[i]] = Value(resp[i+1])
+		}
+		return re, nil
+	}
+	return nil, makeError(resp, keyStart, keyEnd, limit)
 }
