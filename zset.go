cdbe96623308fb2517442e850d23c25a768361ab
everettjf <everettjf@live.com>
2015-05-23 23:24:51 +0800

    add set

--- zset.go
+++ zset.go
@@ -4,16 +4,10 @@ import (
 	"github.com/seefan/goerr"
 )
 
-//设置 hashmap 中指定 key 对应的值内容.
-//
-//  setName hashmap 的名字
-//  key hashmap 的 key
-//  value key 的值
-//  返回 err，执行的错误
-func (this *Client) Hset(setName, key string, value interface{}) (err error) {
-	resp, err := this.Do("hset", setName, key, this.encoding(value, false))
+func (this *Client) Zset(setName, key string, score int64) (err error) {
+	resp, err := this.Do("zset", setName, key, this.encoding(score, false))
 	if err != nil {
-		return goerr.NewError(err, "Hset %s/%s error", setName, key)
+		return goerr.NewError(err, "Zset %s/%s error", setName, key)
 	}
 
 	if len(resp) > 0 && resp[0] == "ok" {
@@ -22,21 +16,15 @@ func (this *Client) Hset(setName, key string, value interface{}) (err error) {
 	return makeError(resp, setName, key)
 }
 
-//获取 hashmap 中指定 key 的值内容.
-//
-//  setName hashmap 的名字
-//  key hashmap 的 key
-//  返回 value key 的值
-//  返回 err，执行的错误
-func (this *Client) Hget(setName, key string) (value Value, err error) {
-	resp, err := this.Do("hget", setName, key)
+func (this *Client) Zget(setName, key string) (score int64, err error) {
+	resp, err := this.Do("zget", setName, key)
 	if err != nil {
-		return "", goerr.NewError(err, "Hget %s/%s error", setName, key)
+		return 0, goerr.NewError(err, "Zget %s/%s error", setName, key)
 	}
 	if len(resp) == 2 && resp[0] == "ok" {
-		return Value(resp[1]), nil
+		return Value(resp[1]).Int64(), nil
 	}
-	return "", makeError(resp, setName, key)
+	return 0, makeError(resp, setName, key)
 }
 
 //删除 hashmap 中的指定 key，不能通过返回值来判断被删除的 key 是否存在.
