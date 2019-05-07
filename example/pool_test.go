a9535e29481ee9941b21a2be574a00ddbf9210d0
seefan <seefan@126.com>
2019-05-07 17:49:36 +0800

    add b test

--- example/pool_test.go
+++ example/pool_test.go
@@ -42,13 +42,88 @@ func BenchmarkP(b *testing.B) {
 	if err != nil {
 		b.Fatal(err)
 	}
+	b.SetParallelism(10)
 	defer pool.Close()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			if c, e := pool.NewClient(); e == nil {
 				c.Close()
 			} else {
-				b.Error(e)
+				//b.Error(e)
+			}
+		}
+	})
+}
+func BenchmarkP100(b *testing.B) {
+	pool, err := gossdb.NewPool(&conf.Config{
+		Host:             "127.0.0.1",
+		Port:             8888,
+		MinPoolSize:      10,
+		MaxPoolSize:      100,
+		MaxWaitSize:      10000,
+		AcquireIncrement: 5,
+	})
+	b.SetParallelism(100)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer pool.Close()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			if c, e := pool.NewClient(); e == nil {
+				c.Close()
+			} else {
+				//b.Error(e)
+			}
+		}
+	})
+}
+
+func BenchmarkP1000(b *testing.B) {
+	pool, err := gossdb.NewPool(&conf.Config{
+		Host:             "127.0.0.1",
+		Port:             8888,
+		MinPoolSize:      10,
+		MaxPoolSize:      100,
+		MaxWaitSize:      10000,
+		AcquireIncrement: 5,
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.SetParallelism(1000)
+	defer pool.Close()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			if c, e := pool.NewClient(); e == nil {
+				c.Close()
+			} else {
+				//b.Error(e)
+			}
+		}
+	})
+}
+
+func BenchmarkP5000(b *testing.B) {
+	pool, err := gossdb.NewPool(&conf.Config{
+		Host:             "127.0.0.1",
+		Port:             8888,
+		MinPoolSize:      10,
+		MaxPoolSize:      100,
+		MaxWaitSize:      10000,
+		AcquireIncrement: 5,
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer pool.Close()
+	b.SetParallelism(5000)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			if c, e := pool.NewClient(); e == nil {
+				c.Close()
+			} else {
+				//b.Error(e)
 			}
 		}
 	})
